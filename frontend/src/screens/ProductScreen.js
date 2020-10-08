ea0c35ed72872d3928e2e9cd1d18fa94d4862716
“[Nicol <[nicol.saha@gmail.com]>
2020-10-08 18:19:57 +0200

    Add Link and LinkContainer elements

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, Listgroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
+
+const ProductScreen = () => {
+  return <div>Product</div>;
+};
+
+export default ProductScreen;
