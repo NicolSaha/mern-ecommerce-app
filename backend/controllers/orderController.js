8e9e0a1c220ed26d636fb679cfcd7d6834626271
“[Nicol <[nicol.saha@gmail.com]>
2020-11-07 18:13:29 +0100

    Add order pay reducer and action, paypal payment functionality, orders list on profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -53,4 +53,52 @@ const getOrderbyId = asyncHandler(async (request, response) => {
   }
 });
 
-export { addOrderItems, getOrderbyId };
+// @description    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (request, response) => {
+  const order = await Order.findById(request.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      //PAYPAL
+      id: request.body.id,
+      status: request.body.status,
+      update_time: request.body.update_time,
+      email_address: request.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    response.json(updatedOrder);
+  } else {
+    response.status(404);
+    throw new Error('Order not found');
+  }
+});
+
+// @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private
+const getMyOrders = asyncHandler(async (request, response) => {
+  const orders = await Order.find({ user: request.user._id });
+  response.json(orders);
+});
+
+// @desc    Get all orders
+// @route   GET /api/orders
+// @access  Private/Admin
+const getOrders = asyncHandler(async (request, response) => {
+  const orders = await Order.find({}).populate('user', 'id name');
+  response.json(orders);
+});
+
+export {
+  addOrderItems,
+  getOrderbyId,
+  updateOrderToPaid,
+  getOrders,
+  getMyOrders,
+};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,17 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems, getOrderbyId } from '../controllers/orderController.js';
+import {
+  addOrderItems,
+  getOrderbyId,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+} from '../controllers/orderController.js';
 import { protect } from '../middleware/authenticationMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderbyId);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/myorders').get(protect, getMyOrders);
+
 export default router;
